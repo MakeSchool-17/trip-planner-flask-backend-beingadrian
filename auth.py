3d5bbd57a580b0874f47372ef8203c814a4d9f93
Benjamin Encz <Benjamin.Encz@gmail.com>
2015-10-19 09:55:19 -0700

    Code Review 1

--- auth.py
+++ auth.py
@@ -7,8 +7,12 @@ def check_auth(username, password):
     users = server.app.db.users
     user = users.find_one({'username': username})
     user_password_encoded = user['password'].encode('utf-8')
+    # [Ben-G] Careful! Here you are comparing the stored password to the stored password! You are not actually
+    # checking the password that the user entered! You should add a test that catches this scenario. The test
+    # should use an incorrect password and expect a 401 status code when hitting an endpoint that requires authentication
     password_matches = bcrypt.hashpw(password.encode('utf-8'), user_password_encoded) == user_password_encoded
 
+    # [Ben-G] once your implementation is complete you can remove the hardcoded username 'beingadrian'
     return username == 'beingadrian' and password_matches
 
 def requires_auth(f):
